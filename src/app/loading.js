68edfba1925327c859dc5b3815af3b08cd364f02
Vannn03 <90308438+Vannn03@users.noreply.github.com>
2024-05-30 21:34:26 +0700

    add: lazy loader

---
+++ src/app/loading.js
@@ -0,0 +1,9 @@
+const Loading = () => {
+    return (
+        <div className="flex h-[90dvh] items-center justify-center bg-color-primary">
+            <div className="spinner" />
+        </div>
+    )
+}
+
+export default Loading
